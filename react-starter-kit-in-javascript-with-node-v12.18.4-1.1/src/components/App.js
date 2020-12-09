181e6a5394d77d58a01a9f951355960c8affc317
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-09 21:32:24 +0900

    create config

--- react-starter-kit-in-javascript-with-node-v12.18.4-1.1/src/components/App.js
+++ react-starter-kit-in-javascript-with-node-v12.18.4-1.1/src/components/App.js
@@ -1,13 +1,17 @@
 import React , {useState} from 'react';
 import SignIn from "./SignIn";
 import Main from "./Main";
+import config from "../config.json";
+
+console.log({config});
+
 
 export default () => {
 
   const [name ,setName ] = useState("");
   console.log(name);
 
-  if(name === ""){
+  if(config.signInEnabled && name === ""){
     return <SignIn setName={setName}/>;
   } else {
     return <Main name={name} setName={setName} />;
