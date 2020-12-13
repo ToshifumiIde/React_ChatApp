0c56f0d25f7c86d0023d3940e62e2d6b28069c8c
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-13 11:23:20 +0900

    apply grid row

---
+++ chatapp/src/components/MessageInputField.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root:{
+    gridRow:2,
+  },
+});
+
+const MessageInputField = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      MessageInputField
+    </div>
+  )
+}
+
+export default MessageInputField
---
+++ chatapp/src/components/SignIn.js
@@ -0,0 +1,152 @@
+import React , {useState , useEffect} from 'react';
+// import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link 
+        color="inherit" 
+        href="https://twitter.com/toshiprogram" 
+        target="_blank"
+        rel="noopener"
+      >
+        Toshifumi Ide
+      </Link>
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn({setName}) {
+  const classes = useStyles();
+
+  const [ disabled, setDisabled ] = useState(true);
+  const [ string , setString ]= useState("");
+  const [ isComposed, setIsComposed ] = useState(false)
+  console.log({ disabled , string , isComposed });
+
+  useEffect(()=>{
+    const disabled = string === "";
+      setDisabled(disabled)
+  },[string]);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          ようこそ
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="ニックネーム"
+            name="name"
+            autoFocus
+            onChange= { e => {
+              setString(e.target.value);
+            }}
+            onKeyDown={(e)=>{
+              if(isComposed) return;
+
+              if(e.key === "Enter"){
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
+            onCompositionStart={()=>{
+              setIsComposed(true)
+              // console.log("日本語入力が始まりました")
+            }}
+            onCompositionEnd={()=>{
+              setIsComposed(false)
+              // console.log("日本語入力が終わりました")
+            }}
+          />
+          {/* <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          /> */}
+          {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={disabled}
+            onClick={()=>{
+              setName(string);
+            }}
+          >
+            はじめる
+          </Button>
+          {/* <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid> */}
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
