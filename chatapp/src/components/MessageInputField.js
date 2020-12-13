e072f4afbe22f3fe5611b7349ba3c4d0efbeed9e
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-13 11:34:36 +0900

    apply grid to input component

--- chatapp/src/components/MessageInputField.js
+++ chatapp/src/components/MessageInputField.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Avatar , Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root:{
     gridRow:2,
+    margin:"26px",
   },
 });
 
 const MessageInputField = () => {
   const classes = useStyles();
   return (
+
     <div className={classes.root}>
-      MessageInputField
+    <Grid container>
+      <Grid xs={1}>
+        <Avatar />
+      </Grid>
+      <Grid xs={10}>
+        入力
+      </Grid>
+      <Grid xs={1}>
+        ボタン
+      </Grid>
+    </Grid>
     </div>
   )
 }
