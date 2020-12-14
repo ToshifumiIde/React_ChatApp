41327b9560c7bb33b161d9f0a18e79c6d5e8ee82
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-14 21:18:53 +0900

    fetch messages

--- chatapp/src/components/MessageList.js
+++ chatapp/src/components/MessageList.js
@@ -13,23 +13,26 @@ const MessageList = () => {
   const classes = useStyles();
   const [messages , setMessages] = useState([]);
 
-  useEffect(()=>{},[]);
-  messagesRef
+  useEffect(()=>{
+    messagesRef
     .orderByKey()
     .limitToLast(3)
     .on("value",(snapshot) => {
-    const messages = snapshot.val();
+    const messages = snapshot.val();//中身を取り出すにはval()メソッドを使用
+    if (messages === null) return;//中身がからであった場合、後の処理を実行しない
     const entries = Object.entries(messages);
     const newMessages = entries.map((entry) => {
       console.log(entry);
-      const [key , nameAndText] = entry;
       // const key = entry[0];
       // const nameAndText = entry[1]
-      return {key , nameAndText};
-    })
+      const [key ,nameAndText] = entry;//上の2行をまとめた書き方、配列に格納
+      return {key , ...nameAndText};
+    });
     console.log(newMessages);
     setMessages(newMessages);
-  })
+  });
+  },[]);
+  
   return (
     <div className={classes.root}>
       MessageList
